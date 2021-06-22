08e66e6bea7a8454ebb8a60cb3c68d2d503fbac0
Kazumasa Kusaba <kazumasa-git@mail.kusaba.xyz>
2021-06-23 00:31:41 +0900

    Added dependencies with the project structure dialog

--- app/src/main/java/xyz/kusaba/slideshowbackground/sample/MainActivity.java
+++ app/src/main/java/xyz/kusaba/slideshowbackground/sample/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import xyz.kusaba.slideshowbackground.SlideshowBackground;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SlideshowBackground slideshowBackground;
     }
 }
